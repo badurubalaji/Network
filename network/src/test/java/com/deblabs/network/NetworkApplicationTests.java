5ded27ad97ecfa2c746ffd147b3d2553dec239d2
DeBLabs <balaji@deblabs.com>
2023-04-26 21:43:20 +0530

    initial commit

---
+++ network/src/test/java/com/deblabs/network/NetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deblabs.network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
